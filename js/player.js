a6cc6fd489999c7c6e9c657379685f1aacf053d8
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-04-01 15:12:29 -0400

    .

--- js/player.js
+++ js/player.js
@@ -165,27 +165,17 @@ function heatDown() {
 
 $(function(){
 
-    var colors = [
-        '26e000','2fe300','37e700','45ea00','51ef00',
-        '61f800','6bfb00','77ff02','80ff05','8cff09',
-        '93ff0b','9eff09','a9ff07','c2ff03','d7ff07',
-        'f2ff0a','fff30a','ffdc09','ffce0a','ffc30a',
-        'ffb509','ffa808','ff9908','ff8607','ff7005',
-        'ff5f04','ff4f03','f83a00','ee2b00','e52000'
-    ];
-
     var rad2deg = 180/Math.PI;
     var deg = 0;
     var bars = $('#dots');
 
-    for(var i=0;i<colors.length;i++){
+    for(var i=0;i<30.length;i++){
 
         deg = i*12;
 
         // Create the colorbars
 
         $('<div class="colorBar">').css({
-            backgroundColor: '#'+colors[i],
             transform:'rotate('+deg+'deg)',
             top: -Math.sin(deg/rad2deg)*80+100,
             left: Math.cos((180 - deg)/rad2deg)*80+100,
